fef8282b4c59aefcb1f7382b3ace513d05f4559c
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-09-27 16:57:36 -0700

    api search

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,1 +1,34 @@
 var weatherDisplay = document.querySelector("#weather-display");
+
+function weatherFunction () {
+    var searchTerm = document.querySelector("#searchTerm").value; 
+    event.preventDefault();
+    console.log(searchTerm); 
+  
+    // Make a `fetch` request concatenating that search term to the query URL for current weather
+    fetch (
+      'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm +'&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
+    ) 
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })  
+    .then(function(response) {
+      console.log(response.data);
+
+  
+      // Fetch Request
+      fetch (
+        'https://api.openweathermap.org/data/2.5/forecast?q=' + searchTerm +'&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
+      )  
+      .then(function(response) {
+        console.log(response);
+        return response.json();
+      })  
+
+      .then(function(response) {
+        console.log(response.data);
+
+      })
+    })  
+  }
