02e2a548746aa06c1b39d57611506e29d875073f
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-09-27 17:46:45 -0700

    display weather

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,34 +1,37 @@
-var weatherDisplay = document.querySelector("#weather-display");
-
-function weatherFunction () {
-    var searchTerm = document.querySelector("#searchTerm").value; 
+function weatherFunction() {
+    var searchTerm = document.querySelector("#searchTerm").value;
     event.preventDefault();
-    console.log(searchTerm); 
-  
-    // Make a `fetch` request concatenating that search term to the query URL for current weather
-    fetch (
-      'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm +'&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
-    ) 
-    .then(function(response) {
-      console.log(response);
-      return response.json();
-    })  
-    .then(function(response) {
-      console.log(response.data);
+    console.log(searchTerm);
 
-  
-      // Fetch Request
-      fetch (
-        'https://api.openweathermap.org/data/2.5/forecast?q=' + searchTerm +'&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
-      )  
-      .then(function(response) {
-        console.log(response);
-        return response.json();
-      })  
+    //Fetch request
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
+    )
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response.data);
 
-      .then(function(response) {
-        console.log(response.data);
+            var currentDate = moment().format("M/D/YYYY").toString();
+            $("#city-name").html(response.name + " " + currentDate);
+            $("#temp").html("Temp: " + response.main.temp + " °F");
+            $("#speed").html("Wind: " + response.wind.speed + " mph");
+            $("#humidity").html("Humidity: " + response.main.humidity + "%");
+            console.log(response.weather[0].icon);
 
-      })
-    })  
-  }
+
+            fetch(
+                'https://api.openweathermap.org/data/2.5/forecast?q=' + searchTerm + '&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
+            )
+                .then(function (response) {
+                    console.log(response);
+                    return response.json();
+                })
+                .then(function (response) {
+                    console.log(response.data);
+
+                })
+        })
+}
